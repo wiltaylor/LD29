5d0a5aa1ff341677684e33cc21dc5387cf48fcdd
Wil Taylor <wilfridtaylor@gmail.com>
2014-04-26 15:24:25 +1000

    Basic Level generation

---
+++ 29-Beneath The Surface/Assets/LevelGenerator.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject DirtBlock;
+    public GameObject ShieldWall;
+    public int WorldWidth = 100;
+    public int WorldHeight = 100;
+    public float BlockOffSet = 100f;
+    public int BlockWidth = 32;
+    public int BlockHeight = 32;
+
+    private float _xOffset = 0f;
+    private new BoxCollider2D collider;
+    private List<GameObject> _currentBlocks = new List<GameObject>();
+    private List<GameObject> _lastBlocks = new List<GameObject>();
+    
+	// Use this for initialization
+	void Start ()
+	{
+        collider = GetComponent<BoxCollider2D>();
+        StartCoroutine("GenerateWorld");
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+
+        foreach(var i in _lastBlocks)
+            Destroy(i);
+
+        var distance = WorldHeight*BlockHeight/BlockOffSet;
+
+        Player.transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + distance, 0f);
+
+        foreach (var i in _currentBlocks)
+        {
+            try
+            {
+                i.transform.position = new Vector3(i.transform.position.x, i.transform.position.y + distance, 0f);
+            }
+            catch (Exception)
+            {
+                //Skip exceptions. It will be destroyed objects.
+            }
+        }
+
+        _lastBlocks = _currentBlocks;
+        _currentBlocks = new List<GameObject>();
+        StartCoroutine("GenerateWorld");
+    }
+
+
+    IEnumerator GenerateWorld()
+    {
+
+        collider.size = new Vector2((BlockWidth * WorldWidth) / BlockOffSet, BlockHeight / BlockOffSet);
+        transform.position = new Vector3(-(BlockWidth / 2) / BlockOffSet, (BlockHeight * WorldHeight / BlockOffSet) / 2, 0f);
+        _xOffset = ((BlockWidth*WorldWidth) / BlockOffSet) / 2;
+        _xOffset = -_xOffset;
+
+        var World = new int[WorldWidth, WorldHeight];
+
+        for (var y = 0; y < WorldHeight; y++)
+        {
+            for (var x = 0; x < WorldWidth; x++)
+                World[x, y] = 0;
+
+            World[0, y] = 1;
+            World[WorldWidth -1, y] = 1;            
+        }
+
+        for (var y = 0; y < WorldHeight; y++)
+        {
+            for (var x = 0; x < WorldWidth; x++)
+            {
+                GameObject newBlock = null;
+
+                if (World[x, y] == 0)
+                    newBlock = (GameObject) Instantiate(DirtBlock);
+                if (World[x, y] == 1)
+                    newBlock = (GameObject) Instantiate(ShieldWall);
+
+                if (newBlock == null)
+                {
+                    Debug.Log(string.Format("Block at {0}/{1} is set to invalid type.", x, y));
+                    newBlock = (GameObject) Instantiate(DirtBlock);
+                }
+
+                newBlock.transform.position = new Vector3(_xOffset + (x*BlockWidth)/BlockOffSet,
+                    (y*BlockHeight)/BlockOffSet, 0f);
+
+                _currentBlocks.Add(newBlock);
+            }
+
+            yield return 0;
+        }
+    }
+}
--- 29-Beneath The Surface/Assets/Scripts/PlayerMovment.cs
+++ 29-Beneath The Surface/Assets/Scripts/PlayerMovment.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class PlayerMovment : MonoBehaviour {
 
-    public float MoveSpeed = 0.1f;
-    public float MaxSpeed = 1f;
-    public float RotateSpeed = 0.1f;
+    public float MoveForce = 0.1f;
+    public float RotateSpeed = 1f;
+    public float MaxLeftAngle = 275f;
+    public float MaxRightAngle = 75f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,18 +16,40 @@ public class PlayerMovment : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(transform.rotation);
-
         if (Input.GetAxis("Horizontal") > 0f)
         {
-            transform.Rotate(0, 0,MaxSpeed);
-            rigidbody2D.AddForce(new Vector2(MoveSpeed, 0f));
+            transform.Rotate(0, 0,RotateSpeed);
+
+            rigidbody2D.AddForce(new Vector2(MoveForce, 0f));
+
+            Debug.Log(transform.eulerAngles); //90
         }
 
         if (Input.GetAxis("Horizontal") < 0f)
         {
-            transform.Rotate(0, 0, -MaxSpeed);
-            rigidbody2D.AddForce(new Vector2(-MoveSpeed, 0f));
+            transform.Rotate(0, 0, -RotateSpeed);
+            rigidbody2D.AddForce(new Vector2(-MoveForce, 0f));
+
+            Debug.Log(transform.eulerAngles); //260
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (transform.eulerAngles.z > MaxRightAngle && transform.eulerAngles.z < MaxLeftAngle)
+        {
+
+            if (transform.eulerAngles.z - MaxLeftAngle > MaxRightAngle - transform.eulerAngles.z)
+            {
+                transform.eulerAngles = new Vector3(0, 0, MaxLeftAngle);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, MaxRightAngle);
+            }
+
+            
         }
     }
 }
