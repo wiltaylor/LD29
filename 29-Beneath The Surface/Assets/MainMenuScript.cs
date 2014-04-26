4c3f81c6dea3e6df19f203ceaa08d2fcc8bcd5f6
Wil Taylor <wilfridtaylor@gmail.com>
2014-04-26 21:15:55 +1000

    Very rough menu

---
+++ 29-Beneath The Surface/Assets/MainMenuScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    private bool howToPlayer = false;
+
+    public void OnGUI()
+    {
+        if (howToPlayer)
+        {
+            GUI.BeginGroup(new Rect(500, 0, 400, 400));
+            GUI.Box(new Rect(0,0, 400, 400), "How to play");
+            GUI.EndGroup();
+        }
+
+        if(GUI.Button(new Rect(100, 100, 100, 40), "New Game"))
+        {
+            Debug.Log("New Game");
+            Application.LoadLevel("MainGame");
+        }
+
+        if (GUI.Button(new Rect(100, 150, 100, 40), "How To Play"))
+        {
+            howToPlayer = !howToPlayer;
+        }
+
+        if (Application.platform == RuntimePlatform.OSXWebPlayer ||
+            Application.platform == RuntimePlatform.WindowsWebPlayer) return;
+
+        if (GUI.Button(new Rect(100, 200, 100, 40), "Exit"))
+        {
+            Application.Quit();
+        }
+    }
+}
